10f2fb1618bb2afa83f02460f00c23779f8dc666
Mikail Sadic <mikail.sadic@utexas.edu>
2024-02-27 22:55:19 -0600

    V1.0
    Hopefully finished and working lab

--- Lab4/ECE319K_Lab4main.c
+++ Lab4/ECE319K_Lab4main.c
@@ -23,6 +23,12 @@
 #define WEST_YELLOW (1<<7)
 #define WEST_RED (1<<8)
 
+#define WESTSWITCH (1<<15)
+#define SOUTHSWITCH (1<<16)
+#define WALKSWITCH (1<<17)
+
+#define WALK_LED_ALL (1<<26)| (1<<27) | (1<<22)
+#define WALK_LED_RED (1<<26)
 
 // put both EIDs in the next two lines
 const char EID1[] = "MFS2447"; //  ;replace abc123 with your EID
@@ -41,33 +47,55 @@ When all inputs true, ... South, Walk, West, South, Walk, West, South, Walk, Wes
 void Traffic_Init(void){ // assumes LaunchPad_Init resets and powers A and B
 
     //South LEDs
-    IOMUX->SECCFG.PINCM[PB0INDEX] = 0x0040081; //Green
-    IOMUX->SECCFG.PINCM[PB1INDEX] = 0x0040081; //Yellow
-    IOMUX->SECCFG.PINCM[PB2INDEX] = 0x0040081; //Red
+    IOMUX->SECCFG.PINCM[PB0INDEX] = 0x0000081; //Green
+    IOMUX->SECCFG.PINCM[PB1INDEX] = 0x0000081; //Yellow
+    IOMUX->SECCFG.PINCM[PB2INDEX] = 0x0000081; //Red
 
     //West LEDs
-    IOMUX->SECCFG.PINCM[PB6INDEX] = 0x0040081; //Green
-    IOMUX->SECCFG.PINCM[PB7INDEX] = 0x0040081; //Yellow
-    IOMUX->SECCFG.PINCM[PB8INDEX] = 0x0040081; //Red
+    IOMUX->SECCFG.PINCM[PB6INDEX] = 0x0000081; //Green
+    IOMUX->SECCFG.PINCM[PB7INDEX] = 0x0000081; //Yellow
+    IOMUX->SECCFG.PINCM[PB8INDEX] = 0x0000081; //Red
 
     //Switches
-    IOMUX->SECCFG.PINCM[PB15INDEX] = 0x0000081; //West
-    IOMUX->SECCFG.PINCM[PB16INDEX] = 0x0000081; //South
-    IOMUX->SECCFG.PINCM[PB17INDEX] = 0x0000081; //Walk
+    IOMUX->SECCFG.PINCM[PB15INDEX] = 0x0040081; //West
+    IOMUX->SECCFG.PINCM[PB16INDEX] = 0x0040081; //South
+    IOMUX->SECCFG.PINCM[PB17INDEX] = 0x0040081; //Walk
 
     //Enable
-    GPIOB->DOE31_0 = SOUTH_GREEN | SOUTH_YELLOW | SOUTH_RED | WEST_GREEN | WEST_YELLOW | WEST_RED;
+    GPIOB->DOESET31_0 = SOUTH_GREEN | SOUTH_YELLOW | SOUTH_RED | WEST_GREEN | WEST_YELLOW | WEST_RED;
 
 }
+//Debugging
+void LEDS_ON(void) {
+    GPIOB->DOUTSET31_0 = SOUTH_GREEN | SOUTH_YELLOW | SOUTH_RED | WEST_GREEN | WEST_YELLOW | WEST_RED;
+}
+//Debugging
+void LEDS_OFF(void) {
+    GPIOB->DOUTCLR31_0 = SOUTH_GREEN | SOUTH_YELLOW | SOUTH_RED | WEST_GREEN | WEST_YELLOW | WEST_RED;
+}
+
+void WALK_ON(void) {
+    GPIOB->DOUTSET31_0 = WALK_LED_ALL;
+}
+
+void WALK_RED(void) {
+    GPIOB->DOUTCLR31_0 = WALK_LED_RED;
+}
+
+void WALK_OFF(void) {
+    GPIOB->DOUTCLR31_0 = WALK_LED_ALL;
+}
+
 /* Activate LEDs
 * Inputs: data1,data2,data3
 *   specify what these means
 * Output: none
 * Feel free to change this. But, if you change the way it works, change the test programs too
 * Be friendly*/
-void Traffic_Out(uint32_t data1, uint32_t south, uint32_t walk){
+void Traffic_Out(uint32_t prev, uint32_t st, uint32_t red){
 
-    // :(
+    GPIOB->DOUTCLR31_0 = prev;
+    GPIOB->DOUTSET31_0 = st | red;
 
 }
 /* Read sensors
@@ -79,20 +107,72 @@ void Traffic_Out(uint32_t data1, uint32_t south, uint32_t walk){
 uint32_t Traffic_In(void){
 
     uint32_t data;
-    data = GPIOB->DIN31_0; //Read input
-    //I think we need to write some extra stuff here
+    data = (GPIOB->DIN31_0 & 0x00038000)>>15;
 
  return data; // return inputs
 }
 
+struct State {
+    uint32_t State;
+    uint32_t Out;
+    uint32_t WaitTime;
+    uint32_t Next[8];
+};
+typedef const struct State St_t;
+
+#define goSouth 0
+#define yellowSouth 1
+#define waitSouth 2
+#define goWest 3
+#define yellowWest 4
+#define waitWest 5
+#define goWalk 6
+#define flashRed1 7
+#define flashOff1 8
+#define flashRed2 9
+#define flashOff2 10
+#define flashRed3 11
+
+/*states
+ * 1 000 = none
+ * 2 001 = south-press
+ * 3 010 = west-press
+ * 4 011 = south&west
+ * 5 100 = walk-press
+ * 6 101 = south&walk
+ * 7 110 = west&walk
+ * 8 111 = all
+ */
+
+#define WHITE WALK_LED_ALL
+#define RED1 WALK_LED_RED
+#define OFF1 WALK_LED_ALL
+#define RED2 WALK_LED_RED
+#define OFF2 WALK_LED_ALL
+#define RED3 WALK_LED_RED
 
 
+St_t FSM[12]= {
+               {0, SOUTH_GREEN | WEST_RED, 200, {goSouth, goSouth, yellowSouth, yellowSouth, yellowSouth, yellowSouth, yellowSouth, yellowSouth}},
+               {1, SOUTH_YELLOW | WEST_RED, 100, {waitSouth, waitSouth, waitSouth, waitSouth, waitSouth, waitSouth, waitSouth, waitSouth}},
+               {2, SOUTH_RED | WEST_RED, 50, {goWalk, goWalk, goWest, goWest, goWalk, goWalk, goWalk, goWalk}},
 
+               {3, WEST_GREEN | SOUTH_RED, 200, {goWest, yellowWest, goWest, yellowWest, yellowWest, yellowWest, yellowWest, yellowWest }},
+               {4, WEST_YELLOW | SOUTH_RED, 100, {waitWest, waitWest, waitWest, waitWest, waitWest, waitWest, waitWest, waitWest}},
+               {5, WEST_RED | SOUTH_RED, 50, {goWalk, goSouth, goWalk, goSouth, goWalk, goWalk, goWalk, goSouth}},
+
+               {6, WHITE | WEST_RED | SOUTH_RED, 200, {goWalk, flashRed1, flashRed1, flashRed1, goWalk, flashRed1, flashRed1, flashRed1}},
+               {7, RED1 | WEST_RED | SOUTH_RED, 50, {flashOff1, flashOff1, flashOff1, flashOff1, flashOff1, flashOff1, flashOff1, flashOff1}},
+               {8, OFF1 | WEST_RED | SOUTH_RED, 50, {flashRed2, flashRed2, flashRed2, flashRed2, flashRed2, flashRed2, flashRed2, flashRed2}},
+               {9, RED2 | WEST_RED | SOUTH_RED, 50, {flashOff2, flashOff2, flashOff2, flashOff2, flashOff2, flashOff2, flashOff2, flashOff2}},
+               {10, OFF2 | WEST_RED | SOUTH_RED, 50, {flashRed3, flashRed3, flashRed3, flashRed3, flashRed3, flashRed3, flashRed3, flashRed3}},
+               {11, RED3 | WEST_RED | SOUTH_RED, 50, {goSouth, goSouth, goWest, goWest, goSouth, goSouth, goWest, goWest}},
+};
 
 // use main1 to determine Lab4 assignment
 void Lab4Grader(int mode);
 void Grader_Init(void);
-int main(void){ // main1
+int main1(void){ // main1
   Clock_Init80MHz(0);
   LaunchPad_Init();
   Lab4Grader(0); // print assignment, no grading
@@ -117,8 +197,16 @@ int main2(void){ // main2
     //write debug code to test your Traffic_Out
 	// Call Traffic_Out testing all LED patterns
 	// Lab 3 dump to record output values
-	
-    Clock_Delay(40000000); // 0.5s
+      LEDS_ON();
+      Clock_Delay(40000000); // 0.5s
+      LEDS_OFF();
+      Clock_Delay(40000000); // 0.5s
+      WALK_ON();
+      Clock_Delay(40000000); // 0.5s
+      WALK_RED();
+      Clock_Delay(40000000); // 0.5s
+      WALK_OFF();
+      Clock_Delay(40000000); // 0.5s
     if((GPIOB->DOUT31_0&0x20) == 0){
       UART_OutString("DOUT not friendly\n\r");
     }
@@ -148,8 +236,14 @@ int main3(void){ // main3
 }
 // use main4 to debug using your dump
 // proving your machine cycles through all states
+
 int main4(void){// main4
-uint32_t input;
+    uint32_t input;
+    uint32_t south;
+    uint32_t west;
+    uint32_t walk;
+    uint32_t St = goSouth;
+    uint32_t prev = goSouth;
   Clock_Init80MHz(0);
   LaunchPad_Init();
   LaunchPad_LED1off();
@@ -160,19 +254,35 @@ uint32_t input;
   UART_OutString("Lab 4, Spring 2024, Step 3. Debug FSM cycle\n\r");
   UART_OutString("EID1= "); UART_OutString((char*)EID1); UART_OutString("\n\r");
   UART_OutString("EID2= "); UART_OutString((char*)EID2); UART_OutString("\n\r");
-// initialize your FSM
+   // initialize your FSM
   SysTick_Init();   // Initialize SysTick for software waits
-
   while(1){
     // 1) output depending on state using Traffic_Out
     // call your Debug_Dump logging your state number and output
     // 2) wait depending on state
     // 3) hard code this so input always shows all switches pressed
     // 4) next depends on state and input
+      //GPIOB->DOUT31_0 = (GPIOB->DOUT31_0&(~0x3F))|FSM[St].Out;
+          Traffic_Out(FSM[prev].Out, FSM[St].Out, WALK_LED_RED);
+          SysTick_Wait10ms(FSM[St].WaitTime);
+          input = Traffic_In();
+          south = (input & 0x02)>>1;
+          west = (input & 0x01)<<1;
+          walk = (input & 0x04);
+          input = south | west | walk;
+          prev = St;
+          St = FSM[St].Next[input];
   }
 }
 // use main5 to grade
-int main5(void){// main5
+
+int main(void){// main5
+    uint32_t input;
+    uint32_t south;
+    uint32_t west;
+    uint32_t walk;
+    uint32_t St = goSouth;
+    uint32_t prev = goSouth;
   Clock_Init80MHz(0);
   LaunchPad_Init();
   Grader_Init();  // execute this line before your code
@@ -187,6 +297,15 @@ int main5(void){// main5
     // 2) wait depending on state
     // 3) input from switches 
     // 4) next depends on state and input
+      Traffic_Out(FSM[prev].Out, FSM[St].Out, WALK_LED_RED);
+      SysTick_Wait10ms(FSM[St].WaitTime);
+      input = Traffic_In();
+      south = (input & 0x02)>>1;
+      west = (input & 0x01)<<1;
+      walk = (input & 0x04);
+      input = south | west | walk;
+      prev = St;
+      St = FSM[St].Next[input];
   }
 }
 
